4982df1be82c99c2fb973b0d249315e93f039e94
dr3ams <dreamer_777@ukr.net>
2024-03-19 15:18:41 +0200

    config wipe

--- kubejs/server_scripts/tags.js
+++
@@ -1,9 +0,0 @@
-ServerEvents.tags('item', event => {
-		
-		event.get('rad3:coins_dim')
-		 .add('kubejs:aether_coin')
-		 .add('kubejs:undergarden_coin')
-		 .add('kubejs:nether_coin')
-		 .add('kubejs:twilight_coin')
-		 .add('kubejs:bumblezone_coin')
-})
