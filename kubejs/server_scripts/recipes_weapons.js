f14839f95777ab39f7bffab54f748fc028608aa9
TheFlame52 <samuelmoughton@gmail.com>
2026-01-26 16:41:11 -0500

    recipes for rad weaponry
    except the revenant's edge because i just can't figure out what to do for that one

--- kubejs/server_scripts/recipes_weapons.js
+++ kubejs/server_scripts/recipes_weapons.js
@@ -236,6 +236,306 @@ ServerEvents.recipes((event) => {
     'simplyswords:runic_cutlass',
     'celestial_core:fire_essence'
   )
-
+  
+  
+  event.shaped(
+  Item.of('radweaponry:hummingbirdfan_blue'), [
+    'WAW',
+	'FFA',
+    'RFW'
+	], {
+	R: 'ntrials:breeze_rod',
+	A: 'the_bumblezone:windy_air',
+	F: 'minecraft:feather',
+	W: 'minecraft:blue_wool'
+  })
+  event.shaped(
+  Item.of('radweaponry:hummingbirdfan_orange'), [
+    'WAW',
+	'FFA',
+    'RFW'
+	], {
+	R: 'ntrials:breeze_rod',
+	A: 'the_bumblezone:windy_air',
+	F: 'minecraft:feather',
+	W: 'minecraft:orange_wool'
+  })
+  event.shaped(
+  Item.of('radweaponry:hummingbirdfan_pink'), [
+    'WAW',
+	'FFA',
+    'RFW'
+	], {
+	R: 'ntrials:breeze_rod',
+	A: 'the_bumblezone:windy_air',
+	F: 'minecraft:feather',
+	W: 'minecraft:pink_wool'
+  })
+  event.shaped(
+  Item.of('radweaponry:hummingbirdfan_purple'), [
+    'WAW',
+	'FFA',
+    'RFW'
+	], {
+	R: 'ntrials:breeze_rod',
+	A: 'the_bumblezone:windy_air',
+	F: 'minecraft:feather',
+	W: 'minecraft:purple_wool'
+  })
+  event.shaped(
+  Item.of('radweaponry:hummingbirdfan_red'), [
+    'WAW',
+	'FFA',
+    'RFW'
+	], {
+	R: 'ntrials:breeze_rod',
+	A: 'the_bumblezone:windy_air',
+	F: 'minecraft:feather',
+	W: 'minecraft:red_wool'
+  })
+  event.shaped(
+  Item.of('radweaponry:hummingbirdfan_snow'), [
+    'WAW',
+	'FFA',
+    'RFW'
+	], {
+	R: 'ntrials:breeze_rod',
+	A: 'the_bumblezone:windy_air',
+	F: 'minecraft:feather',
+	W: 'minecraft:white_wool'
+  })
+  event.shapeless(
+  Item.of('radweaponry:hummingbirdfan_blue'),
+  [
+    '#rad3:fan',
+    'minecraft:blue_dye'
+  ])
+  event.shapeless(
+  Item.of('radweaponry:hummingbirdfan_orange'),
+  [
+    '#rad3:fan',
+    'minecraft:orange_dye'
+  ])
+  event.shapeless(
+  Item.of('radweaponry:hummingbirdfan_pink'),
+  [
+    '#rad3:fan',
+    'minecraft:pink_dye'
+  ])
+  event.shapeless(
+  Item.of('radweaponry:hummingbirdfan_purple'),
+  [
+    '#rad3:fan',
+    'minecraft:purple_dye'
+  ])
+  event.shapeless(
+  Item.of('radweaponry:hummingbirdfan_red'),
+  [
+    '#rad3:fan',
+    'minecraft:red_dye'
+  ])
+  event.shapeless(
+  Item.of('radweaponry:hummingbirdfan_snow'),
+  [
+    '#rad3:fan',
+    'minecraft:white_dye'
+  ])
+  event.shaped(
+  Item.of('radweaponry:hydra_scythe'), [
+    'INS',
+	'ISE',
+    'SE '
+	], {
+	I: 'cataclysm:black_steel_ingot',
+	N: 'minecraft:netherite_ingot',
+	S: '#forge:rods/wooden',
+	E: 'dungeonsdelight:spider_extract'
+  })
+  event.shaped(
+  Item.of('radweaponry:halbird'), [
+    ' IF',
+	' SI',
+    'SI '
+	], {
+	I: 'embers:silver_ingot',
+	S: '#forge:rods/wooden',
+	F: 'minecraft:feather'
+  })
+  event.shaped(
+  Item.of('radweaponry:holy_spear'), [
+    ' DI',
+	' SD',
+    'S  '
+	], {
+	I: 'l2complements:totemic_gold_ingot',
+	S: '#forge:rods/wooden',
+	D: 'minecraft:glowstone_dust'
+  })
+  event.shaped(
+  Item.of('radweaponry:scorpion_blade'), [
+    ' IE',
+	'IEI',
+    'NI '
+	], {
+	I: 'embers:lead_ingot',
+	N: 'minecraft:netherite_ingot',
+	E: 'dungeonsdelight:spider_extract'
+  })
+  event.shaped(
+  Item.of('radweaponry:hemovial'), [
+    'GCG',
+	'GBG',
+    'GGG'
+	], {
+	C: 'minecraft:chain',
+	G: '#forge:glass_panes',
+	B: 'bloodmagic:life_essence_bucket'
+  })
+  event.recipes.ars_nouveau.enchanting_apparatus(
+    [
+      "ars_nouveau:source_gem_block",
+      "ars_nouveau:source_gem_block",
+      "ars_nouveau:source_gem_block",
+      "bosses_of_mass_destruction:ancient_anima",
+      "celestial_core:midnight_fragment",
+      "celestial_core:midnight_fragment",
+    ],
+    "simplyswords:iron_claymore",
+    "radweaponry:starhawk"
+  );
+  event.shaped(
+  Item.of('radweaponry:cinderstorm'), [
+    'EEE',
+	'BCB',
+    ' I '
+	], {
+	C: 'minecraft:crossbow',
+	E: 'celestial_core:fire_essence',
+	I: 'minecraft:netherite_ingot',
+	B: 'hmag:burning_core'
+  })
+  event.shapeless(
+  Item.of('radweaponry:power_pole'),
+  [
+    '9x naturesaura:ancient_stick'
+  ])
+  event.shaped(
+  Item.of('radweaponry:pitcher_canteen'), [
+    'P P',
+	'PRP',
+    ' P '
+	], {
+	P: 'minecraft:pitcher_plant',
+	R: 'minecraft:redstone_block'
+  })
+  event.shapeless(
+  Item.of('radweaponry:bestiary_charm'),
+  [
+    'araxers_bestiary:bestiary_book',
+	'minecraft:ender_pearl',
+	'minecraft:ghast_tear',
+	'minecraft:phantom_membrane',
+	'#forge:heads',
+	'minecraft:emerald',
+	'hmag:evil_crystal_fragment',
+	'hmag:soul_powder',
+	'graveyard:corruption'
+  ])
+  event.shaped(
+  Item.of('radweaponry:orb_fire'), [
+    'JEJ',
+	'ECE',
+    'JEJ'
+	], {
+	J: 'minecraft:blaze_powder',
+	E: 'celestial_core:fire_essence',
+	C: 'bosses_of_mass_destruction:blazing_eye'
+  })
+  event.shaped(
+  Item.of('radweaponry:soluna_blade'), [
+    ' NI',
+	'MIN',
+    'SL '
+	], {
+	L: 'celestial_core:light_fragment',
+	M: 'celestial_core:midnight_fragment',
+	I: 'embers:silver_ingot',
+	S: '#forge:rods/wooden',
+	N: 'embers:silver_nugget'
+  })
+  event.shaped(
+  Item.of('radweaponry:rhythmweaver'), [
+    'PPP',
+	'TLT',
+    'PDP'
+	], {
+	L: 'xercamusic:lyre',
+	T: 'apotheosis:warden_tendril',
+	P: 'minecraft:prismarine_shard',
+	D: 'minecraft:diamond'
+  })
+  event.shaped(
+  Item.of('radweaponry:lightning_hammer'), [
+    'INI',
+	'LSL',
+    ' S '
+	], {
+	I: 'minecraft:iron_ingot',
+	N: 'minecraft:netherite_ingot',
+	S: '#forge:rods/wooden',
+	L: 'hmag:lightning_particle'
+  })
+  event.shaped(
+  Item.of('radweaponry:gunblade'), [
+    'PP ',
+	'GII',
+    'O  '
+	], {
+	G: 'landsoficaria:daedalian_gear',
+	I: 'landsoficaria:sideros_ingot',
+	O: 'landsoficaria:orichalcum_ingot',
+	P: 'landsoficaria:greenpowder'
+  })
+  event.shaped(
+  Item.of('radweaponry:void_core'), [
+    'JEJ',
+	'ECE',
+    'JEJ'
+	], {
+	J: 'deep_aether:skyjade',
+	E: 'celestial_core:void_essence',
+	C: 'bosses_of_mass_destruction:void_thorn'
+  })
+  event.shaped(
+  Item.of('radweaponry:viper_amulet'), [
+    'GGG',
+	'G G',
+    'GFG'
+	], {
+	G: 'minecraft:gold_nugget',
+	F: 'mowziesmobs:naga_fang'
+  })
+  event.shaped(
+  Item.of('radweaponry:gilded_armguard'), [
+    'LG',
+	'CG',
+    'LG'
+	], {
+	G: 'minecraft:gold_ingot',
+	L: 'minecraft:leather',
+	C: 'l2complements:blackstone_core'
+  })
+  event.shaped(
+  Item.of('radweaponry:ravenous_maw'), [
+    'ERE',
+	'BFB',
+    'GFG'
+	], {
+	E: 'minecraft:ender_eye',
+	R: 'minecraft:rotten_flesh',
+	B: 'dungeonsdelight:brined_flesh',
+	G: 'dungeonsdelight:gritty_flesh',
+	F: 'hmag:sharp_fang'
+  })
 
 });
