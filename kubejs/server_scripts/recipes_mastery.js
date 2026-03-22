6f2febf3a25120c121260af5e43921a4724b2a39
dr3ams <dreamer_777@ukr.net>
2026-03-22 08:07:14 +0200

    Update recipes_mastery.js
    added suggestions by tiger

--- kubejs/server_scripts/recipes_mastery.js
+++ kubejs/server_scripts/recipes_mastery.js
@@ -11,33 +11,59 @@ ServerEvents.recipes((event) => {
 
   // enchanter — converts items into dust_experience
   const DISSOLVER = [
-    ['kubejs:scroll_exp',           2, 'kubejs:dust_experience'],
-    ['minecraft:experience_bottle', 1, 'kubejs:dust_experience'],
-    ['minecraft:lapis_lazuli',      2, 'kubejs:dust_experience'],
-    ['minecraft:amethyst_block',    1, 'kubejs:dust_experience'],
-    ['kubejs:spawnercore',          2, 'kubejs:dust_experience'],
-    ['shieldinghealth:power_token', 5, 'kubejs:dust_experience'],
+    ['kubejs:scroll_exp',                        2, 'kubejs:dust_experience'],
+    ['minecraft:experience_bottle',              1, 'kubejs:dust_experience'],
+    ['minecraft:lapis_lazuli',                   2, 'kubejs:dust_experience'],
+    ['minecraft:amethyst_block',                 1, 'kubejs:dust_experience'],
+    ['kubejs:spawnercore',                       2, 'kubejs:dust_experience'],
+    ['shieldinghealth:power_token',              5, 'kubejs:dust_experience'],
+    ['celestial_core:sakura_fragment',           1, 'kubejs:dust_experience'],
+    ['simplehats:hatbag_common',                 1, 'kubejs:dust_experience'],
+    ['simplehats:hatbag_uncommon',               2, 'kubejs:dust_experience'],
+    ['simplehats:hatbag_rare',                   3, 'kubejs:dust_experience'],
+    ['collectorsalbum:common_card_pack',         1, 'kubejs:dust_experience'],
+    ['collectorsalbum:uncommon_card_pack',        2, 'kubejs:dust_experience'],
+    ['collectorsalbum:rare_card_pack',           3, 'kubejs:dust_experience'],
+    ['collectorsalbum:epic_card_pack',           4, 'kubejs:dust_experience'],
+    ['regions_unexplored:hanging_prismarite',    1, 'kubejs:dust_experience'],
   ];
 
   // alchemy — converts items into dust_alchemical
   const TRANSMUTATOR = [
-    ['minecraft:ender_pearl',       2, 'kubejs:dust_alchemical'],
-    ['minecraft:blaze_powder',      2, 'kubejs:dust_alchemical'],
-    ['apotheosis:gem_dust',         2, 'kubejs:dust_alchemical'],
-    ['kubejs:spawnercore',          2, 'kubejs:dust_alchemical'],
-    ['shieldinghealth:power_token', 5, 'kubejs:dust_alchemical'],
+    ['minecraft:ender_pearl',          2, 'kubejs:dust_alchemical'],
+    ['minecraft:blaze_powder',         2, 'kubejs:dust_alchemical'],
+    ['apotheosis:gem_dust',            2, 'kubejs:dust_alchemical'],
+    ['kubejs:spawnercore',             2, 'kubejs:dust_alchemical'],
+    ['shieldinghealth:power_token',    5, 'kubejs:dust_alchemical'],
+    ['ars_nouveau:wilden_horn',        1, 'kubejs:dust_alchemical'],
+    ['4x bonfires:ash_pile',           1, 'kubejs:dust_alchemical'],
+    ['embers:ancient_motive_core',     1, 'kubejs:dust_alchemical'],
+    ['minecraft:poisonous_potato',     1, 'kubejs:dust_alchemical'],
   ];
 
   // salvaging — converts items into scraps
   const SALVAGER = [
-    ['minecraft:ender_pearl',       2, 'kubejs:scraps'],
-    ['minecraft:raw_copper_block',  1, 'kubejs:scraps'],
-    ['minecraft:blaze_powder',      2, 'kubejs:scraps'],
-    ['apotheosis:gem_dust',         2, 'kubejs:scraps'],
-    ['minecraft:gold_block',        1, 'kubejs:scraps'],
-    ['kubejs:spawnercore',          2, 'kubejs:scraps'],
-    ['shieldinghealth:power_token', 5, 'kubejs:scraps'],
+    ['minecraft:ender_pearl',              2, 'kubejs:scraps'],
+    ['minecraft:raw_copper_block',         1, 'kubejs:scraps'],
+    ['minecraft:blaze_powder',             2, 'kubejs:scraps'],
+    ['apotheosis:gem_dust',                2, 'kubejs:scraps'],
+    ['minecraft:gold_block',               1, 'kubejs:scraps'],
+    ['kubejs:spawnercore',                 2, 'kubejs:scraps'],
+    ['shieldinghealth:power_token',        5, 'kubejs:scraps'],
+    ['4x farmersdelight:rope',             1, 'kubejs:scraps'],
+    ['minecraft:name_tag',                 2, 'kubejs:scraps'],
+    ['minecraft:music_disc_13',            3, 'kubejs:scraps'],
+    ['minecraft:music_disc_cat',           3, 'kubejs:scraps'],
+    ['paraglider:spirit_orb',              3, 'kubejs:scraps'],
+    ['simplehats:hatbag_common',           1, 'kubejs:scraps'],
+    ['simplehats:hatbag_uncommon',         2, 'kubejs:scraps'],
+    ['simplehats:hatbag_rare',             3, 'kubejs:scraps'],
+    ['collectorsalbum:common_card_pack',   1, 'kubejs:scraps'],
+    ['collectorsalbum:uncommon_card_pack', 2, 'kubejs:scraps'],
+    ['collectorsalbum:rare_card_pack',     3, 'kubejs:scraps'],
+    ['collectorsalbum:epic_card_pack',     4, 'kubejs:scraps'],
   ];
+
 
   // ──────────────────────────────────────────────
   // SHAPED RECIPES
